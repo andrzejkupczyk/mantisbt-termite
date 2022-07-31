451953d634277c19f62b973aeb9336d3bbfd09fe
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2022-07-31 09:16:52 +0200

    🚚 Rename termite_api to plugin_api

--- core/plugin_api.php
+++ core/plugin_api.php
@@ -6,7 +6,7 @@ if (!function_exists('helper_api_url')) {
     /**
      * Build URL to the given API endpoint within the plugin's route group.
      */
-    function helper_api_url(string $endpoint, string $pluginBaseName = null): string
+    function plugin_api_url(string $endpoint, string $pluginBaseName = null): string
     {
         return helper_mantis_url(vsprintf('api/rest%s/%s', [
             plugin_route_group($pluginBaseName),
